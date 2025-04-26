c25d157148e6924c21a4c0fb820cae374a655625
Kevin Paiz Ramos <paizkevin27@gmail.com>
2025-04-26 15:57:36 -0400

    Routing

--- Ramhacks-project/src/App.jsx
+++ Ramhacks-project/src/App.jsx
@@ -1,9 +1,25 @@
 import { useState } from "react";
 import { useRoutes } from "react-router-dom";
 import "./App.css";
+import LandingPage from "./pages/landing";
+import LoginPage from "./pages/login";
+import SignupPage from "./pages/signup";
 
 function App() {
-  const routes = useRoutes([]);
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+    {
+      path: "/signup",
+      element: <SignupPage />,
+    },
+  ]);
 
   return (
     <>
---
+++ Ramhacks-project/src/pages/landing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1>Welcome to the Landing Page</h1>
+      <p>This is a simple landing page.</p>
+    </div>
+  );
+}
+
+export default LandingPage;
