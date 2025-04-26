85d56d2365421ce63ad749b8e453a3e0e891e607
John Distler <94850886+ZonDizzler@users.noreply.github.com>
2025-04-26 14:01:51 -0400

    initialize firebase

---
+++ Ramhacks-project/src/config/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.REACT_FIREBASE_API_KEY,
+  authDomain: "ramhacks-d7372.firebaseapp.com",
+  projectId: "ramhacks-d7372",
+  storageBucket: "ramhacks-d7372.firebasestorage.app",
+  messagingSenderId: "690716237731",
+  appId: "1:690716237731:web:ce40def3d83c99624bc416",
+  measurementId: "G-6Q8RCYFH9G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
